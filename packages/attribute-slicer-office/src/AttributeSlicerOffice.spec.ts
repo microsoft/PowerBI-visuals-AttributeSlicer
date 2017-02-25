7114c66b46a656d2d777998bbf1c01edfbe0f62d
David Tittsworth <stopyoukid@gmail.com>
2017-02-25 15:48:59 -0800

    Added shell tests

--- packages/attribute-slicer-office/src/AttributeSlicerOffice.spec.ts
+++ packages/attribute-slicer-office/src/AttributeSlicerOffice.spec.ts
@@ -2,4 +2,11 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-
+import "mocha";
+describe("Attribute Slicer Office", () => {
+    it("should filter the underlying dataset when something is selected");
+    it("should restore selection from excel");
+    it("should save selection to excel");
+    it("should restore search text from excel");
+    it("should save search text to excel");
+});
