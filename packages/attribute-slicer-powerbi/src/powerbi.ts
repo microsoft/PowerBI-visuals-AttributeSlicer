b15621cde72e4ea6e018eb6858df362f35ce4eb4
David Tittsworth <stopyoukid@gmail.com>
2017-11-22 08:58:03 -0800

    Fixed some issues that were missed

--- packages/attribute-slicer-powerbi/src/powerbi.ts
+++ packages/attribute-slicer-powerbi/src/powerbi.ts
@@ -1,1 +1,1 @@
-/// <reference path="../node_modules/powerbi-visuals-tools/templates/visuals/.api/v1.8.0/PowerBI-Visuals.d.ts"/>
+import "powerbi-visuals-tools/templates/visuals/.api/v1.8.0/PowerBI-Visuals.d";
